742ccc8e9fd5e703dbb287c0c6459b279a06177a
Jongmin Yoon <jongmin4943@gmail.com>
2021-07-07 20:42:14 +0900

    first commit

---
+++ attach/src/main/java/org/thesix/attach/AttachApplication.java
@@ -0,0 +1,13 @@
+package org.thesix.attach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttachApplication.class, args);
+    }
+
+}
---
+++ funding/src/test/java/org/thesix/funding/FundingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thesix.funding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
