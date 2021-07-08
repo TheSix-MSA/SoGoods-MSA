977e550b9a4e9d5209f15c2ea4322afa6e758554
chnew12 <jaey1229@gmail.com>
2021-07-08 19:43:35 +0900

    Board CRUD list, search, paging 추가해야함

--- board/src/test/java/org/thesix/board/BoardApplicationTests.java
+++ board/src/test/java/org/thesix/board/BoardApplicationTests.java
@@ -1,7 +1,9 @@
 package org.thesix.board;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.thesix.board.repository.BoardRepository;
 
 @SpringBootTest
 class BoardApplicationTests {
