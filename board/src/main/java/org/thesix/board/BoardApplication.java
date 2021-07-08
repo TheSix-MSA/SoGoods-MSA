db321e5278ec9e12ac7895e125707737e2af56f2
Jongmin Yoon <jongmin4943@gmail.com>
2021-07-08 20:37:31 +0900

    gateway and eureka tested success

--- board/src/main/java/org/thesix/board/BoardApplication.java
+++ board/src/main/java/org/thesix/board/BoardApplication.java
@@ -2,13 +2,11 @@ package org.thesix.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class BoardApplication {
 
     public static void main(String[] args) {
--- member/src/main/java/org/thesix/member/MemberApplication.java
+++ member/src/main/java/org/thesix/member/MemberApplication.java
@@ -2,10 +2,9 @@ package org.thesix.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class MemberApplication {
 
     public static void main(String[] args) {
