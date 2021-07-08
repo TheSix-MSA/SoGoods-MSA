5ff3e9e76db04a610af55ecc4348837e6dca52c9
Jongmin Yoon <jongmin4943@gmail.com>
2021-07-08 20:18:43 +0900

    testing GateWay and Eureka

--- board/src/main/java/org/thesix/board/BoardApplication.java
+++ board/src/main/java/org/thesix/board/BoardApplication.java
@@ -3,10 +3,12 @@ package org.thesix.board;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BoardApplication {
 
     public static void main(String[] args) {
