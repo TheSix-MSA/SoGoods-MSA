7e51658ce36ddcc1366c06ba09486819e870d45e
chnew12 <jaey1229@gmail.com>
2021-07-09 20:43:54 +0900

    Board Paging 거의 다 함

--- board/src/main/java/org/thesix/board/controller/BoardController.java
+++ board/src/main/java/org/thesix/board/controller/BoardController.java
@@ -5,10 +5,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.thesix.board.dto.BoardDTO;
+import org.thesix.board.dto.BoardListDTO;
 import org.thesix.board.service.BoardService;
 
 @Controller
@@ -20,16 +19,16 @@ public class BoardController {
     private final BoardService boardService;
 
     // 게시판 글등록("/board/register")
-    @PostMapping("/register")
-    public ResponseEntity<Long> register(@RequestBody BoardDTO dto) {
-        Long bno = boardService.register(dto);
-        log.info("Register BNO:" + bno);
-        return ResponseEntity.ok().body(bno);
+    @PostMapping("/{type}/register")
+    public ResponseEntity<BoardDTO> register(@RequestBody BoardDTO dto, @PathVariable String type) {
+        BoardDTO registerDTO = boardService.register(dto);
+        log.info("Register BNO:" + registerDTO);
+        return ResponseEntity.ok().body(registerDTO);
     }
 
     // 게시판 글수정("/board/modify/{bno}")
-    @PutMapping("/modify/{bno}")
-    public ResponseEntity<BoardDTO> modify(@PathVariable Long bno, @RequestBody BoardDTO dto) {
+    @PutMapping("/{type}/modify/{bno}")
+    public ResponseEntity<BoardDTO> modify(@PathVariable Long bno, @PathVariable String type, @RequestBody BoardDTO dto) {
         dto.setBno(bno);
         log.info("Modify BNO: " + bno);
         BoardDTO modifyDTO = boardService.modify(dto);
@@ -37,8 +36,8 @@ public class BoardController {
     }
 
     // 게시판 글삭제("/board/remove/{bno}")
-    @DeleteMapping("/remove/{bno}")
-    public ResponseEntity<BoardDTO> removed(@PathVariable Long bno, @RequestBody BoardDTO dto) {
+    @DeleteMapping("/{type}/remove/{bno}")
+    public ResponseEntity<BoardDTO> removed(@PathVariable Long bno, @PathVariable String type, @RequestBody BoardDTO dto) {
         dto.setBno(bno);
         BoardDTO removedDTO = boardService.remove(dto);
         log.info("Remove BNO: " + bno);
@@ -46,14 +45,20 @@ public class BoardController {
     }
 
     // 게시판 특정 글조회("/board/read/{bno}")
-    @GetMapping("/read/{bno}")
-    public ResponseEntity<BoardDTO> read(@PathVariable Long bno) {
+    @GetMapping("/{type}/read/{bno}")
+    public ResponseEntity<BoardDTO> read(@PathVariable Long bno, @PathVariable String type) {
         BoardDTO readDTO  = boardService.read(bno);
         log.info("Read DTO: " + readDTO);
         return ResponseEntity.ok().body(readDTO);
     }
 
     // 게시판 글목록("/board/list")
-    // @GetMapping("/list")
+    @GetMapping("/{type}/list")
+    public ResponseEntity<BoardDTO> getList(@PathVariable String type) {
+        BoardDTO boardListDTO = boardService.boardList(type);
+        log.info("BoardListDTO: " + boardListDTO);
+        return ResponseEntity.ok().body(boardListDTO);
+    }
+
 
 }
