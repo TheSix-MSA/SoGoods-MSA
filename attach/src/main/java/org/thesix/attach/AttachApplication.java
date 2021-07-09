b17fae54d211c3e79d770eee798d01d33fc3497a
wonjin <wonjin1776@naver.com>
2021-07-09 20:23:20 +0900

    업로드 확정API 완료

--- attach/src/main/java/org/thesix/attach/AttachApplication.java
+++ attach/src/main/java/org/thesix/attach/AttachApplication.java
@@ -2,8 +2,10 @@ package org.thesix.attach;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AttachApplication {
 
     public static void main(String[] args) {
--- attach/src/main/java/org/thesix/attach/dto/AttachConfimRequestDTO.java
+++ attach/src/main/java/org/thesix/attach/dto/AttachConfimRequestDTO.java
@@ -7,11 +7,23 @@ import java.util.List;
 @Data
 public class AttachConfimRequestDTO {
 
+    /*
+{
+    "tableName": "tbl_board",
+    "keyValue": "123",
+    "tempFileNameList": [
+        "9561eea4-32b7-4871-a1d6-164b1b9622ab_%ED%95%9C%EA%B8%80.png",
+        "s_9561eea4-32b7-4871-a1d6-164b1b9622ab_%ED%95%9C%EA%B8%80.png"
+    ],
+    "mainFileName": "9561eea4-32b7-4871-a1d6-164b1b9622ab_%ED%95%9C%EA%B8%80.png"
+}
+     */
+
     private String tableName;
 
-    private String keyValue;
+    private Long keyValue;
 
-    private List<String> fileNameList;
+    private List<String> tempFileNameList;
 
     private String mainFileName;
 }
