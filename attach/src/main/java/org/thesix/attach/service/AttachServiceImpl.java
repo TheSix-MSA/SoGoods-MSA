e1887af6b64adf7fae0873e430cd7c62c192c6f6
wonjin <wonjin1776@naver.com>
2021-07-14 01:06:58 +0900

    DELETE기능 업데이트, 업로드폴더생성 추가

--- attach/src/main/java/org/thesix/attach/service/AttachServiceImpl.java
+++ attach/src/main/java/org/thesix/attach/service/AttachServiceImpl.java
@@ -1,21 +1,35 @@
 package org.thesix.attach.service;
 
+import com.sun.jdi.InternalException;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnailator;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.multipart.MultipartFile;
 import org.thesix.attach.dto.AttachConfimRequestDTO;
+import org.thesix.attach.dto.UploadResultDTO;
 import org.thesix.attach.entity.Attach;
 import org.thesix.attach.repository.AttachRepository;
 
 import javax.swing.*;
+import javax.transaction.Transactional;
 import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -26,6 +40,55 @@ public class AttachServiceImpl implements AttachService{
     private String uploadPath;
 
     private final AttachRepository attachRepository;
+
+    @Override
+    public List<UploadResultDTO> uplaodtemp(MultipartFile[] files) {
+        List<UploadResultDTO> resultDTOList = new ArrayList<>();
+
+        for(MultipartFile file : files){
+
+            if(file.getContentType().startsWith("image") == false) {
+//              return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+            }
+
+            //브라우저별 파일 오리지널파일이름 처리
+            String originalName = file.getOriginalFilename();
+            int pos =  originalName.lastIndexOf("\\") + 1;
+            String fileName = originalName.substring(pos);
+            log.info(fileName);
+            //날짜 폴더 생성
+            //String folderPath = makeFolder();
+
+            //UUID
+            String uuid = UUID.randomUUID().toString();
+
+            //저장할 파일 이름 중간에 "_"
+            String commonPath = uploadPath + File.separator + "temp" + File.separator;
+            String uuidFileName =  uuid + "_" + fileName;
+
+            String severSideOriginFilePath = commonPath + uuidFileName;
+
+            Path savePath = Paths.get(severSideOriginFilePath);
+
+            try{
+                //서버에 저장
+                //  1. 원본
+                file.transferTo(savePath);
+                //  2. 썸네일
+                String serverSideThumbnailFilePath = commonPath + "s_" + uuidFileName;
+                File thumbnailFile = new File(serverSideThumbnailFilePath);
+                Thumbnailator.createThumbnail(savePath.toFile(), thumbnailFile, 100, 100);
+
+                //응답해줄 업로드 정보 추가
+                resultDTOList.add(new UploadResultDTO(uuid, fileName));
+            }catch (IOException e){
+                throw new InternalException();
+            }
+        }
+
+        return resultDTOList;
+    }
 
     @Override
     public void registerConfimedImages(AttachConfimRequestDTO requestDTO) throws UnsupportedEncodingException {
@@ -80,4 +143,56 @@ public class AttachServiceImpl implements AttachService{
             attachRepository.save(attach);
         }
     }
+
+    @Override
+    public ResponseEntity<byte[]> getFile(String opt, String filename) {
+        ResponseEntity<byte[]> result = null;
+
+        try {
+
+            String srcFileName = URLDecoder.decode(filename, "UTF-8");
+
+            File file = new File(uploadPath + File.separator + opt + File.separator + srcFileName);
+
+            HttpHeaders headers = new HttpHeaders();
+
+            headers.add("Content-Type", Files.probeContentType(file.toPath()));
+
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), headers, HttpStatus.OK);
+
+        }catch (Exception e) {
+            throw new InternalException();
+        }
+        return result;
+    }
+
+    @Override
+    @Transactional
+    public void removeTempFile(String opt,String fileName) {
+        try{
+            String srcFileName = null;
+
+            srcFileName = URLDecoder.decode(fileName);
+
+            File originFile   = new File(uploadPath + File.separator + opt + File.separator + srcFileName);
+            File thumnailFile = new File(uploadPath + File.separator + opt + File.separator + "s_" + srcFileName);
+
+            originFile.delete();
+            thumnailFile.delete();
+        }catch(Exception e){
+            throw new InternalException();
+        }
+        System.out.println("fileName: " + fileName);
+        if(opt.equals("saved")){
+            try {
+                attachRepository.deleteByOriginalName(URLEncoder.encode(fileName, "UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+
 }
